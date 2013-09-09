06e4e5d6b96054d2c34c24d819a4053f6663c1ad
Nikhil Garg <nikhgarg93@gmail.com>
2013-09-08 21:31:48 -0500

    Fixed Joglekar's numerous mistakes

--- Lab1/OLED_1968/fixed.c
+++ Lab1/OLED_1968/fixed.c
@@ -21,19 +21,34 @@ outTestCaseType outTests3[16]={
 { 32768,  "128.00" }, //  32768/256 = 128
 { 34567,  "135.02" }, //  34567/256 = 135.02
 {123456,  "482.25" }, // 123456/256 = 482.25
-{255998,  "999.99" }, // 255998/256 = 999.99
-{256000,  "***.**" }  // error
+{99999,  "999.99" }, // 255998/256 = 999.99
+{99999,  "***.**" }  // error
 };
+
+outTestCaseType outTests2[10]={ 
+{     -100000, " *.***"},
+{-10000, " *.***"},
+{-9999, "-9.999"},
+{-999, "-0.999"},
+{-1 ,"-0.001"},
+{0 ," 0.000"},
+{123, " 0.123"},
+{1234, " 1.234"},
+{9999, " 9.999"},
+{10000, " *.***"} // error
+};
+
 unsigned int Errors,AnError;
 char Buffer[10];
 void main(void){ // possible main program that tests your functions
 unsigned int i;
   Errors = 0;
-  for(i=0; i<16; i++){
-    Fixed_uBinOut8s(outTests3[i].InNumber,Buffer);
-    if(strcmp(Buffer, outTests3[i].OutBuffer)){
+  for(i=0; i<10; i++){
+		Fixed_sDecOut3s(outTests2[i].InNumber, Buffer);
+	
+    if(strcmp(Buffer, outTests2[i].OutBuffer)){
       Errors++;
-      AnError = i;
+     AnError = i;
     }
   }
   for(;;) {} /* wait forever */
@@ -47,9 +62,9 @@ void Fixed_uDecOut2s(unsigned long n,  char *string)
 		int rem = n%100;
 		int div = n/100;
 		char * pre;
-		if (div < 1000)
+		if (n < 1000)
 			pre = "  ";
-		else if (div < 10000)
+		else if (n < 10000)
 			pre = " ";
 		else pre = "";
 		
@@ -63,25 +78,43 @@ void Fixed_uDecOut2s(unsigned long n,  char *string)
 void Fixed_sDecOut3s(long n, char *string)
 {
 	if( n >= 10000 || n <= -10000)
-		sprintf(string, "***.**");
+		sprintf(string, " *.***");
 	else{
 		int pre = n/1000;
 		int post = n%1000;
+  	if (post < 0) post = -post;
 		if(n<0)
-			sprintf(string, "-%d.%d", pre, post);
+			sprintf(string, "-%1d.%03d", -pre, post);
 		else
-			sprintf(string, " %d.%d", pre, post);
+			sprintf(string, " %1d.%03d", pre, post);
 	}
 }
 
 void Fixed_uBinOut8s(unsigned long n,  char *string){
 	int d = n*100/256;
-	if(d < 1000)
-		sprintf(string, "  %d.%d", d/100, d%100);
-	else if(d<10000)
-		sprintf(string, " %d.%d", d/100, d%100);
-	else if(d<100000)
-		sprintf(string, "%d.%d", d/100, d%100);
-	else 
-		sprintf(string, "***.**");
+	if (d%100 < 10){
+		if (d < 10)
+			sprintf(string, "  %d.0%d", d/100, d%100);
+		else if(d < 1000)
+			sprintf(string, "  %d.0%d", d/100, d%100);
+		else if(d<10000)
+			sprintf(string, " %d.0%d", d/100, d%100);
+		else if(d<100000)
+			sprintf(string, "%d.0%d", d/100, d%100);
+		else 
+			sprintf(string, "***.**");
+	}
+	else{
+
+		if(d < 1000)
+			sprintf(string, "  %d.%d", d/100, d%100);
+		else if(d<10000)
+			sprintf(string, " %d.%d", d/100, d%100);
+		else if(d<100000)
+			sprintf(string, "%d.%d", d/100, d%100);
+		else 
+			sprintf(string, "***.**");
+		
+		
+	}
 }
