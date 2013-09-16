c1a51077c74152d9571c7d67120649b7a1ca31ae
Nikhil Garg <nikhgarg93@gmail.com>
2013-09-16 09:30:22 -0500

    wrote debugging part G, added for lab 3 starter

---
+++ Lab3 Logo_1968/logo.d
@@ -0,0 +1,2 @@
+logo.o: logo.c
+logo.o: rit128x96x4.h
---
+++ Lab3 Logo_1968/logotestmain.d
@@ -0,0 +1,6 @@
+logotestmain.o: LogoTestMain.c
+logotestmain.o: ..\inc\hw_types.h
+logotestmain.o: ..\driverlib\sysctl.h
+logotestmain.o: ..\inc\lm3s1968.h
+logotestmain.o: rit128x96x4.h
+logotestmain.o: logo.h
